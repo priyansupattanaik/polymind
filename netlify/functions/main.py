f4ac9789329884b0e733e9f47a1d4b9f3a5d0b35
Priyansu Pattanaik <priyansupattanaikwork@gmail.com>
2026-02-12 03:38:04 +0530

    netlify.toml fixed v5

--- netlify/functions/main.py
+++ netlify/functions/main.py
@@ -1,8 +1,5 @@
 from mangum import Mangum
 from app.main import app
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
-
+# Entry point for Netlify Functions
 handler = Mangum(app)
